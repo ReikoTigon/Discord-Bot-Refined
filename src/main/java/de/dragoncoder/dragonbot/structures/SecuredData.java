058e69e65bfadbc6319d2d4263f2ddf42c6f95fe
Reiko Tigon <40246719+ReikoTigon@users.noreply.github.com>
2020-08-05 13:09:06 +0200

    Made minimalsm real :D
    Removed unnecessary filds from SecuredData.java

--- src/main/java/de/dragoncoder/dragonbot/structures/SecuredData.java
+++ src/main/java/de/dragoncoder/dragonbot/structures/SecuredData.java
@@ -7,9 +7,5 @@ import lombok.Getter;
 public class SecuredData {
 
     private final String token; //Discord Application Token
-    private final String link; //Invite Link of the bot
-    private final String mysqlLink; //Link to the Bot MySQL Database
-    private final String mysqlUser; //User for the Databases
-    private final String mysqlPswd; //Password for the databases
     
 }
